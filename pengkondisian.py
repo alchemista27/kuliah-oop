d4c6a9caf74db30afa1093c00daa33fc51c12d4f
alchemista27 <septian.j@gmail.com>
2021-04-05 21:31:59 +0700

    Penambahan perintah control flow

---
+++ pengkondisian.py
@@ -0,0 +1,17 @@
+#ini adalah kode untuk control flow
+#dalam kategori pengulangan
+#ada beberapa perintah: if; elif dan else
+a = "mobil"
+if a == "motor":
+    print ("saya suka motor")
+else:
+    print ("saya suka naik angkot")
+b = "pensil"
+if b == "pensil":
+    print ("saya mau beli pensil")
+elif b == "buku":
+    print ("saya harus beli buku")
+elif b == "pena":
+    print ("saya harus beli pena")
+else:
+    print ("saya mau makan gado-gado")
--- pengulangan.py
+++ pengulangan.py
@@ -1,4 +1,12 @@
-nmr = [1,2,3,4,5,6,7,8]
-
-for _ in nmr:
-    print ('saya ganteng')
+#pengulangan adalah perintah untuk mengulang suatu pernyataan tertentu
+#ada dua perintah yang dapat kita gunakan: for loops dan while loops
+num = [1, 2, 3, 4, 5]
+for a in num:
+    print ("saya dapat nilai A")
+for _ in num:
+    print ("saya mau makan Udang")
+#while = selama, selama satu kondisi benar maka akan tetap diulang
+x = 0
+while x < 5:
+    print (f"nilai x saat ini adalah {x}")
+    x=x+1
